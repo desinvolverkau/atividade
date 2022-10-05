fc0883381299432a725d093db9e79f596a20b2a4
desinvolverkau <114266284+desinvolverkau@users.noreply.github.com>
2022-10-04 21:47:15 -0300

    Create atidade1a8.js

---
+++ atidade1a8.js
@@ -0,0 +1,18 @@
+ NUmero1 
+for(num = 1 ; num <= 100; num++)
+console.log(num)
+
+Numero2
+
+let num1 = Number(prompt("Digite o primeiro numero"))
+let num2 = Number(prompt("Digite o segunda numero"))
+
+for (num= num1 ; num <= num2; num++){
+
+
+  console.log (num)
+
+
+}
+
+3 a 8 em andamento....
